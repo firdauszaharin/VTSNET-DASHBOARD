264e9066aef18e54b30a14a69c317d75ccf299cd
Ts.Muhammad Firdaus <firdauszaharin@gmail.com>
2026-03-06 15:37:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -132,7 +132,7 @@ PDF_COL = "UPLOAD REPORT"
 
 # AIS sheet (anda perlu set gid yang betul untuk setiap sheet)
 # buat sementara, saya letak gid=0 utk kedua-dua; tukar ikut sheet sebenar anda
-AISVDES_URL = "https://docs.google.com/spreadsheets/d/1HQUV7NXuhAKtKW-weSwAmhIMOde8CZM8XiTiaF1P7K4/export?format=csv&gid=0"
+AISVDES_URL = "https://docs.google.com/spreadsheets/d/1HQUV7NXuhAKtKW-weSwAmhIMOde8CZM8XiTiaF1P7K4/edit?usp=sharing"
 AISVTS_URL  = "https://docs.google.com/spreadsheets/d/1HQUV7NXuhAKtKW-weSwAmhIMOde8CZM8XiTiaF1P7K4/export?format=csv&gid=0"
 
 # ========= 4. DATA LOAD FUNCTIONS =========
