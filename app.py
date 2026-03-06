31a3c05137c286c106911895be7420ee8b8ef27d
Ts.Muhammad Firdaus <firdauszaharin@gmail.com>
2026-03-06 15:40:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -188,10 +188,6 @@ if "selected_row_idx" not in st.session_state:
 
 # ========= 6. SIDEBAR =========
 with st.sidebar:
-    # Logo
-    if os.path.exists("logo.png"):
-        st.image("logo.png", use_container_width=True)
-    else:
         st.markdown("### GreenFinder VTMS")
 
     st.divider()
